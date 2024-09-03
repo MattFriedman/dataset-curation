bb9791a1e08d482e079fce18c7ba349c1fb0d76c
Matt Friedman <matt.friedman@gmail.com>
2024-09-03 06:39:46 -0400

    wip

--- routes/auth.js
+++ routes/auth.js
@@ -2,28 +2,27 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const User = require('../models/User');
-// const roleAccess = require('../middleware/rbac');
 
-const { roleAccess, isAuthenticated } = require('../middleware/rbac');
+const {roleAccess} = require('../middleware/rbac');
 
 // Register (admin only)
 router.post('/register', roleAccess(['admin']), async (req, res) => {
     try {
-        const { username, password, roles } = req.body;
-        const existingUser = await User.findOne({ username });
+        const {username, password, roles} = req.body;
+        const existingUser = await User.findOne({username});
         if (existingUser) {
             return res.status(400).send('Username already exists');
         }
-        
+
         // Ensure roles is an array
         const userRoles = Array.isArray(roles) ? roles : [roles];
-        
+
         // Ensure at least 'user' role is assigned
         if (!userRoles.includes('user')) {
             userRoles.push('user');
         }
 
-        const user = new User({ username, password, roles: userRoles });
+        const user = new User({username, password, roles: userRoles});
         await user.save();
         res.redirect('/login');
     } catch (error) {
@@ -53,7 +52,9 @@ router.post('/login', (req, res, next) => {
 // Logout
 router.get('/logout', (req, res, next) => {
     req.logout((err) => {
-        if (err) { return next(err); }
+        if (err) {
+            return next(err);
+        }
         res.redirect('/');
     });
 });
@@ -61,7 +62,7 @@ router.get('/logout', (req, res, next) => {
 // Change Password
 router.post('/change-password', async (req, res) => {
     try {
-        const { currentPassword, newPassword } = req.body;
+        const {currentPassword, newPassword} = req.body;
         const user = await User.findById(req.user.id);
 
         // Check if the current password is correct
